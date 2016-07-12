37770d61c0aef659e8fe9a72985cbe19256a0538
root <root@43637.simplecloud.club>
2016-07-12 16:20:33 +0000

    123

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'dbname' => '3roles',
+        'host' => 'localhost',
+        'user' => 'root',
+        'prefix' => 'pk_',
+        'password' => 'JbBDmuMMpW1pNKunYkUVdtyF',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'eRNJGk7RYZ1QfNuAGJjDts4IzWh8yntbZDDJEG7rVb7gMJ4yZSsoL4SyahaOjNGb',
+  ),
+);
